b38b8fb74ee64312f2ac50edf849ef3727cb3fdc
amdjad4real <amdjad4real@users.noreply.github.com>
2026-06-23 21:15:11 +0100

    Add Firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID'
+  apiKey: 'AIzaSyANUdkKuYHcs_4eS2EmR9ZD9YtW7-M3JwM',
+  authDomain: 'mk-clients.firebaseapp.com',
+  projectId: 'mk-clients',
+  storageBucket: 'mk-clients.firebasestorage.app',
+  messagingSenderId: '1060309066308',
+  appId: '1:1060309066308:web:9438b3bc6162025f801211',
+  measurementId: 'G-5SBLC4LDQC'
 };
 
 const app = initializeApp(firebaseConfig);
